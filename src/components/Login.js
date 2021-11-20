c9be88534d5ab2bd19ed7f8a651b0767a13953b8
Shishira Bhat O <shishirabhato@gmail.com>
2021-11-20 23:07:26 +0530

    Designed the new login page in form of cards

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
-import { Button, Form } from 'react-bootstrap'
+import { Button, Form ,Card, Container} from 'react-bootstrap'
 import axios from 'axios'
 import { Link, useNavigate } from 'react-router-dom';
 import './Login.css'
+import background from "./tile_background.jpeg";
 
 const Login = () => {
     const navigate = useNavigate()
@@ -70,63 +71,78 @@ const Login = () => {
     }
 
     return (
+        <div style={{
+            backgroundImage: `url(${background})`,
+            backgroundRepeat: "repeat",
+            minHeight: "100vh"
+        }}>
         <div className="body">
+            <Container>
+            <Card className="loginCard p-4" style={{ borderRadius: '0.5rem',flex:'1',minWidth: '22rem',maxWidth: '30rem'}}>
             <div className="login">
                 <h1>Login</h1>
                  <Form>
                     <Form.Group className="mb-3" controlId="formBasicEmail">
-                        <Form.Label>Email address</Form.Label>
-                        <Form.Control onChange={handleChange} type="email" placeholder="Enter email" name="email" />
+                        <Form.Label className="loginEmail" style={{ maxWidth: '20rem'}}>Email address</Form.Label>
+                        <Form.Control onChange={handleChange} type="email" placeholder="Enter email" name="email" style={{ maxWidth: '20rem'}}/>
                         <Form.Text className="text-muted">
                             We'll never share your email with anyone else.
                         </Form.Text>
                     </Form.Group>
                     
                     <Form.Group className="mb-3" controlId="formBasicPassword">
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control onChange={handleChange} type="password" placeholder="Password" name="password" />
+                        <Form.Label className="loginPassword" style={{ maxWidth: '20rem'}}>Password</Form.Label>
+                        <Form.Control onChange={handleChange} type="password" placeholder="Password" name="password" style={{ maxWidth: '20rem'}} />
                     </Form.Group>
-                    <Button variant="primary" type="submit" onClick={handleLoginClick}>
+                    <Button className="button" variant="primary" type="submit" onClick={handleLoginClick}>
                         Submit
                     </Button>
                 </Form> 
             </div>
+            </Card>
+            </Container>
+            <Container>
+            <Card className="signupCard p-4" style={{ borderRadius: '0.5rem',flex:'1',minWidth:'22rem',maxWidth: '30rem'}}>
             <div className="signup" >
                 <h1>SignUp</h1>
                 <Form>
                     <Form.Group className="mb-3" controlId="formBasicEmail">
-                        <Form.Label>Email address</Form.Label>
-                        <Form.Control onChange={handleChange} type="email" placeholder="Enter email" name="email" required />
+                        <Form.Label className="signUpEmail" style={{ maxWidth: '20rem'}}>Email address</Form.Label>
+                        <Form.Control onChange={handleChange} type="email" placeholder="Enter email" name="email" style={{ maxWidth: '20rem'}} required />
                         <Form.Text className="text-muted">
                             We'll never share your email with anyone else.
                         </Form.Text>
                     </Form.Group>
 
                     <Form.Group className="mb-3" controlId="formBasicText">
-                        <Form.Label>Name</Form.Label>
-                        <Form.Control onChange={handleChange} type="text" placeholder="Name" name="name"  required/>
+                        <Form.Label className="signUpName" style={{ maxWidth: '20rem'}}>Name</Form.Label>
+                        <Form.Control onChange={handleChange} type="text" placeholder="Name" name="name"  style={{ maxWidth: '20rem'}} required/>
                     </Form.Group>
 
                     <Form.Group className="mb-3" controlId="formBasicText">
-                        <Form.Label>Phone Number: </Form.Label>
-                        <Form.Control onChange={handleChange} type="phone" placeholder="Phone" name="phone" />
+                        <Form.Label className="signUpPh" style={{ maxWidth: '20rem'}}>Phone Number: </Form.Label>
+                        <Form.Control onChange={handleChange} type="phone" placeholder="Phone" name="phone" style={{ maxWidth: '20rem'}} />
                     </Form.Group>
 
                     <Form.Group className="mb-3" controlId="formBasicPassword">
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control onChange={handleChange} type="password" placeholder="Password" name="password"  required/>
+                        <Form.Label className="signUpPassword" style={{ maxWidth: '20rem'}}>Password</Form.Label>
+                        <Form.Control onChange={handleChange} type="password" placeholder="Password" name="password" style={{ maxWidth: '20rem'}}  required/>
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="formBasicCheckbox">
                         <Form.Check type="checkbox" label="Check me out" />
                     </Form.Group>
                     <Link to="/home">
-                        <Button variant="primary" type="submit" onClick={handleSignupClick}>
+                        <Button className="submit" variant="primary" type="submit" onClick={handleSignupClick}>
                             Submit
                         </Button>
                     </Link>
                 </Form>
             </div>
+            </Card>
+            </Container>
         </div >
+        </div>
+        
     );
 };
 
